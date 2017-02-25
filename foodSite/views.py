910ce167f6e9ee53f7cf1bb6e83a7f515f110123
apurv1205 <apurv.1205@gmail.com>
2017-02-25 23:22:02 +0530

    ...

--- foodSite/views.py
+++ foodSite/views.py
@@ -5,15 +5,23 @@ from django.views.decorators.csrf import csrf_protect
 from django.shortcuts import render_to_response,render
 from django.http import HttpResponseRedirect
 from django.template import RequestContext
- 
+from foodSite.models import *
+
 @csrf_protect
 def register(request):
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
         if form.is_valid():
             stat = form.cleaned_data['choice_field']
-            if stat=="Costumer" : status="C"
-            else : status="R" 
+            if stat=="Costumer" : 
+                status="C"
+                C=Customer(name=form.cleaned_data['first_name'],passwd=form.cleaned_data['password1'],email=form.cleaned_data['email'],contact=form.cleaned_data['username'])
+                C.save()
+            else : 
+                status="R"
+                R=Restaurant(name=form.cleaned_data['first_name'],passwd=form.cleaned_data['password1'],email=form.cleaned_data['email'],contact=form.cleaned_data['username'])
+                R.save() 
+
             user = User.objects.create_user(
             first_name=form.cleaned_data['first_name'],
             last_name=status,
